ad919b22a7e8b7440f9ea09ba2441f7ad26f46c1
Tega <beniyoke@gmail.com>
2024-03-11 16:06:01 +0100

    Create users table

---
+++ db/migrations/20240311150328_create-accounts-table.ts
@@ -0,0 +1,19 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("Users", function (table){
+        table.increments("id").primary();
+        table.string("first_name").notNullable();
+        table.string("last_name").notNullable();
+        table.string("email").notNullable();
+        table.string("password").notNullable();
+        table.timestamps(true, true);
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable("Users");
+}
+
